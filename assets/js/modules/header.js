e700bba00229949776474fbf971e1dba66fe5c83
Jeffersondeab <82956161+Jeffersondeab@users.noreply.github.com>
2022-05-03 14:26:31 -0300

    add

--- assets/js/modules/header.js
+++ assets/js/modules/header.js
@@ -1,11 +1,10 @@
 const header = document.querySelector('[data-header]')
 
 
-
 function setListeners(){
     window.addEventListener('scroll', function(){
             if(window.scrollY > 20){
-                header.style.backgroundColor = ' red'   /*  #0C0D14 */
+                header.style.backgroundColor  = 'red'   /*  #0C0D14 */
             }else{
                 header.style.backgroundColor = 'transparent'
         }
