31a90576f490d2a10cfd4bc61e7a2ef0466351c6
Jeffersondeab <82956161+Jeffersondeab@users.noreply.github.com>
2022-05-04 00:22:28 -0300

    add

--- assets/js/modules/header.js
+++ assets/js/modules/header.js
@@ -6,6 +6,8 @@ function setListeners(){
     window.addEventListener('scroll', function(){
             if(window.scrollY > 20){
                 header.style.backgroundColor  = 'red'   /*  #0C0D14 */
+            }else{
+                header.style.backgroundColor = 'transparent'
         }
     })
 }
