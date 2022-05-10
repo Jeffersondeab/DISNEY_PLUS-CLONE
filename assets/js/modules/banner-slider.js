cb3049c9305090a304c982724ccbfc4119e886b7
Jeffersondeab <82956161+Jeffersondeab@users.noreply.github.com>
2022-05-10 15:33:57 -0300

    add

--- assets/js/modules/banner-slider.js
+++ assets/js/modules/banner-slider.js
@@ -1,4 +1,8 @@
 const sliderItems = document.querySelectorAll('[data-banner="item"]')
+const slider = document.querySelector('[data-banner="slider"]')
+const state = {
+    mouseDownPosition: 0
+}
 
 function preventDefault(event){
     event.preventDefault()
@@ -6,13 +10,25 @@ function preventDefault(event){
 
 
 function onMouseDown(event){
-    console.log('testes')    
+    const slide = event.currentTarget
+    state.mouseDownPosition = event.clientX
+    console.log('mousedown -', event.clientX)
+    slide.addEventListener('mousemove', onMouseMove)  
 }
+
 function onMouseMove(event){
-    console.log('testes')
+    slider.style.transform = `translatex(${event.clientX - state.mouseDownPosition}px)`
+    console.log('mousemove -', event.clientX)
 }
+
 function onMouseUp(event){
-    console.log('testes')
+    const slide = event.currentTarget
+    slide.removeEventListener('mousemove', onMouseMove)
+}
+
+function onMouseLeave(event){
+    const slide = event.currentTarget
+    slide.removeEventListener('mousemove', onMouseMove)
 }
 
 
@@ -22,10 +38,12 @@ function setListeners(){
             link.addEventListener('click', preventDefault)
             slide.addEventListener('dragstart', preventDefault)
             slide.addEventListener('mousedown', onMouseDown)
-            slide.addEventListener('mouseup', onMouseMove)
-            slide.addEventListener('mousemove', onMouseUp)
+            slide.addEventListener('mouseup', onMouseUp)
+            slide.addEventListener('mouseleave', onMouseLeave)
+           
     })
 }
+
 
 function init() {
     setListeners()
