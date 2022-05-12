a44bc436f1d4c6bedc312032058b446ce8449de1
Jeffersondeab <82956161+Jeffersondeab@users.noreply.github.com>
2022-05-12 10:54:27 -0300

    add

--- assets/js/modules/banner-slider.js
+++ assets/js/modules/banner-slider.js
@@ -2,6 +2,15 @@ const sliderItems = document.querySelectorAll('[data-banner="item"]')
 const slider = document.querySelector('[data-banner="slider"]')
 const state = {
     mouseDownPosition: 0
+    lastTranslatePosition:0,
+    movementPosition:0,
+    currentSliderPosition:0
+    currentSlideIndex:0
+}
+
+function translateSlide(position){
+    state.lastTranslatePosition = position
+    slider.style.transform = `translatex(${position}px)`
 }
 
 function preventDefault(event){
@@ -9,20 +18,35 @@ function preventDefault(event){
 }
 
 
-function onMouseDown(event){
+function onMouseDown(event, index){
     const slide = event.currentTarget
     state.mouseDownPosition = event.clientX
+    state.currentSliderPosition = event.clientX - state.lastTranslatePosition
+    state.currentSlideIndex = index
+    console.log(state.currentSlideIndex)
     console.log('mousedown -', event.clientX)
     slide.addEventListener('mousemove', onMouseMove)  
 }
 
 function onMouseMove(event){
-    slider.style.transform = `translatex(${event.clientX - state.mouseDownPosition}px)`
-    console.log('mousemove -', event.clientX)
+    state.movementPosition = event.clientX - state.mouseDownPosition
+    console.log(state.movementPosition)
+    translateSlide(event.clientX - state.currentSliderPosition)
+    //console.log('mousemove -', event.clientX)
 }
 
 function onMouseUp(event){
     const slide = event.currentTarget
+    if(state.movementPosition > 150){
+        const calc = slide.offsetWidth * (state.currentSlideIndex - 1)
+        translateSlide(-calc)
+    }else if(state.movementPosition < -150){
+        const calc = slide.offsetWidth * (state.currentSlideIndex + 1)
+        translateSlide(-calc)
+    }else{
+        const calc = slide.offsetWidth * (state.currentSlideIndex)
+        translateSlide(-calc)
+    }
     slide.removeEventListener('mousemove', onMouseMove)
 }
 
@@ -37,7 +61,8 @@ function setListeners(){
             const link = slide.querySelector('.banner-slider__link')
             link.addEventListener('click', preventDefault)
             slide.addEventListener('dragstart', preventDefault)
-            slide.addEventListener('mousedown', onMouseDown)
+            slide.addEventListener('mousedown', function(event) {onMouseDown(event, index)
+            })
             slide.addEventListener('mouseup', onMouseUp)
             slide.addEventListener('mouseleave', onMouseLeave)
            
