1a47c98c02ab2718b3fbefef47a3be5c79d9e726
Jeffersondeab <82956161+Jeffersondeab@users.noreply.github.com>
2022-05-24 12:43:17 -0300

    add

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,10 @@
 import header from './modules/header.js'
 import bannerSlider from './modules/banner-slider.js' 
+import collection from './modules/collection.js'
 
 header.init()
 bannerSlider.init()
+collection.init()
 
  
 
---
+++ assets/js/modules/collection.js
@@ -0,0 +1,14 @@
+const carouselList = document.querySelector('[data-carousel="list"]')
+const carouselItem = document.querySelectorAll('[data-carousel="item"]')
+const btnPrevious = document.querySelectorAll('[data-carousel="btn-previous"]')
+const btnNext = document.querySelectorAll('[data-carousel="btn-next"]')
+
+
+function init(){
+    console.log('teste')
+}
+
+
+export default{
+    init
+}
