7ab4cc3b0ad2d913a25a5db24534dd3bcd57a869
Jeffersondeab <82956161+Jeffersondeab@users.noreply.github.com>
2022-05-26 20:30:01 -0300

    add

--- assets/js/modules/collection.js
+++ assets/js/modules/collection.js
@@ -1,17 +1,6 @@
-const carouselList = document.querySelector('[data-carousel="list"]')
-const carouselItem = document.querySelectorAll('[data-carousel="item"]')
-const btnPrevious = document.querySelectorAll('[data-carousel="btn-previous"]')
-const btnNext = document.querySelectorAll('[data-carousel="btn-next"]')
-
-
-const state = {
-    mouseDownPosition: 0,
-    movement: 0,
-    lastTranslatePosition:0,
-    currentSlidePosition:0,
-    currentItemIndex:0,
-    currentSlideIndex:0
-}
+const collections = document.querySelectorAll('[data-carousel="collection"]')
+const collectionData = []
+let currentCollectionIndex = 0
 
 const preventDefault = (event) =>{
     event.preventDefault()
@@ -20,59 +9,137 @@ const preventDefault = (event) =>{
 
 
 const translateSlide = (position) => {
+    const {state, carouselList} = collectionData[currentCollectionIndex]
     state.lastTranslatePosition = position
     carouselList.style.transform = `translateX(${position}px)`
 }
 
 const getCenterPosition = (slideIndex) => {
-    const item = carouselItem[state.currentItemIndex]
+    const {state, carouselItems} = collectionData[currentCollectionIndex]
+    const item = carouselItems[state.currentItemIndex]
     const itemWidth = item.offsetWidth
-    const bodywidth = document.body.clientWidth
+    const bodyWidth = document.body.clientWidth
     const slideWidth = itemWidth * 5
-    const margin = (bodywidth - slideWidth) / 2
+    const margin = (bodyWidth - slideWidth) / 2
     return margin - (slideWidth * slideIndex)
 }
 
+const getLastSlideIndex = () => {
+    const {carouselItems} = collectionData[currentCollectionIndex]
+    const lastItemIndex = carouselItems.length - 1
+    return Math.floor(lastItemIndex / 5)
+}
+
+const animateTransition = (active) =>{
+    const {carouselList} = collectionData[currentCollectionIndex]
+    if(active){
+        carouselList.style.transition = 'transform .3s'
+    }else{
+        carouselList.style.removeProperty('transition')
+    }
+}
+
 const setVisibleSlide = (slideIndex) => {
+    const {state} = collectionData[currentCollectionIndex]
+    state.currentSlideIndex = slideIndex
     const centerPosition = getCenterPosition(slideIndex)
+    animateTransition(true)
     translateSlide(centerPosition)
 }
 
-const onMouseDown = (event, index) =>{
+const backwardSlide = () => {
+    const {state} = collectionData[currentCollectionIndex]
+    if(state.currentSlideIndex > 0){
+        setVisibleSlide(state.currentSlideIndex - 1)
+    }else{
+        setVisibleSlide(state.currentSlideIndex)
+    }
+}
+
+const forwardSlide = () => {
+    const {state} = collectionData[currentCollectionIndex]
+    const lastSlideIndex = getLastSlideIndex()
+    if(state.currentSlideIndex < lastSlideIndex){
+        setVisibleSlide(state.currentSlideIndex + 1)
+    }else{
+        setVisibleSlide(state.currentSlideIndex)
+    }
+}
+
+
+const onMouseDown = (event, itemIndex) =>{
+    const {state} = collectionData[currentCollectionIndex]
     const item = event.currentTarget
-    state.currentItemIndex = index
+    state.currentItemIndex = itemIndex
     state.mouseDownPosition = event.clientX
     state.currentSlidePosition = event.clientX - state.lastTranslatePosition
+    animateTransition(false)
     item.addEventListener('mousemove',onMouseMove)
-    console.log('mouse down')
 }
 
 const onMouseMove = (event) =>{
+    const {state} = collectionData[currentCollectionIndex]
     state.movement = event.clientX - state.mouseDownPosition
     const position = event.clientX - state.currentSlidePosition
     translateSlide(position)
-    console.log('mouse move')
 }
 
 const onMouseUp = (event) =>{
+    const {state} = collectionData[currentCollectionIndex]
+    if(state.movement > 150){
+        backwardSlide()
+    }else if(state.movement < -150){
+         forwardSlide()
+    }else{
+        setVisibleSlide(state.currentSlideIndex)
+    }
+
     const item = event.currentTarget
     item.removeEventListener('mousemove',onMouseMove)
-    console.log('mouse up')
 }
  
 const onMouseLeave = (event) =>{
     const item = event.currentTarget
     item.removeEventListener('mousemove',onMouseMove)
-    console.log('mouse leave')
 }
 
-const setListeners = () => {
-    carouselItem.forEach((item, index) => {
+const insertCollectionDate = (collection) => {
+    collectionData.push({
+        carouselList: collection.querySelector('[data-carousel="list"]'),
+        carouselItem: collection.querySelectorAll('[data-carousel="item"]'),
+        btnPrevious: collection.querySelector('[data-carousel="btn-previous"]'),
+        btnNext: collection.querySelector('[data-carousel="btn-next"]'),
+    
+    
+        state: {
+            mouseDownPosition: 0,
+            movement: 0,
+            lastTranslatePosition:0,
+            currentSlidePosition:0,
+            currentItemIndex:0,
+            currentSlideIndex:0
+        }
+    })
+}
+
+const setListeners = (collectionIndex) => {
+    const {btnNext, btnPrevious, carouselItem} = collectionData[collectionIndex]
+    btnNext.addEventListener('click', () => {
+        currentCollectionIndex = collectionIndex
+        forwardSlide()
+    })
+    
+    btnPrevious.addEventListener('click', () => {
+        currentCollectionIndex = collectionIndex
+        backwardSlide()
+    })
+    carouselItem.forEach((item, itemIndex) => {
         const link = item.querySelector('.movie-carousel__link')
         link.addEventListener('click', preventDefault)
         item.addEventListener('dragstart', preventDefault)
         item.addEventListener('mousedown', (event) => {
-            onMouseDown(event, index)
+            currentCollectionIndex = collectionIndex
+            onMouseDown(event, itemIndex)
         })
         item.addEventListener('mouseup', onMouseUp)
         item.addEventListener('mouseleave', onMouseLeave)
@@ -80,6 +147,10 @@ const setListeners = () => {
 }
 
 const init = () => {
+    collections.forEach((collection, collectionIndex) => {
+        insertCollectionDate(collection)
+        setListeners(collectionIndex)
+    })
     setListeners()
     setVisibleSlide(0)
 }
