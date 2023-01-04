3b8bd954abfed3c27aced7816642b011d4426d0f
Jeffersondeab <82956161+Jeffersondeab@users.noreply.github.com>
2023-01-04 16:23:56 -0300

    add

--- assets/js/componentes/control-slider-item.js
+++ assets/js/componentes/control-slider-item.js
@@ -1,7 +1,7 @@
-const ControlSliderItem = () =>{
+const ControlSliderItem = () => {
     return `
-    <button class="controls-slider__item" data-banner="btn-control"><i class="fa-solid fa-circle"></i></button> 
+        <button class="controls-slider__item" data-banner="btn-control"></button>
     `
-} 
+}
 
 export default ControlSliderItem
