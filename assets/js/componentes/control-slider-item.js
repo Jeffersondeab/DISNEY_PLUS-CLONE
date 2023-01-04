452fdfc100b0ec9dec2c966e2f1a669f34f0a620
Jeffersondeab <82956161+Jeffersondeab@users.noreply.github.com>
2023-01-04 19:52:15 -0300

    add

--- assets/js/componentes/control-slider-item.js
+++ assets/js/componentes/control-slider-item.js
@@ -1,6 +1,8 @@
 const ControlSliderItem = () => {
     return `
-        <button class="controls-slider__item" data-banner="btn-control"></button>
+        <button class="controls-slider__item" data-banner="btn-control">
+            <i class="fa-solid fa-circle"></i>
+        </button>
     `
 }
 
